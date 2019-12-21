6aff70e4150cbe07df322090613187ab37050111
hamza202 <k8668975@gmail.com>
2019-12-21 21:36:48 +0200

    21/12

---
+++ js/main.js
@@ -0,0 +1,19 @@
+new Mmenu(document.querySelector('#menu'),
+    {
+        drag: false,
+        pageScroll: {
+            scroll: true,
+            update: true
+        },
+        sidebar: {
+            expanded: {
+                use: '(min-width: 1300px)',
+            }
+        },
+        navbars: [
+            {
+                content: ['<div><img src="../images/logo.png"></div>', 'close']
+            }
+        ]
+    }
+);
