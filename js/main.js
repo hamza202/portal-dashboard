a161dd1106cd2d3e38eaae9353d32925a386ce9d
hamza202 <k8668975@gmail.com>
2019-12-31 14:58:23 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ new Mmenu(document.querySelector('#menu'),
         },
         navbars: [
             {
-                content: ['<div><img src="./images/logo.png"></div>', 'close']
+                content: ['<div><a class="d-block" href="index.html"><img src="./images/logo.png"></a></div>', 'close']
             }
         ]
     }
@@ -20,24 +20,3 @@ new Mmenu(document.querySelector('#menu'),
 
 //Date Range
 
-$('.daterange').daterangepicker();
-
-$(function() {
-    $('.dateTimeInput').daterangepicker({
-        timePicker: true,
-        singleDatePicker: true,
-        showDropdowns: true,
-        locale: {
-            format: 'M/DD/YYYY hh:mm A'
-        }
-    });
-});
-$(function() {
-    $('.dateInput').daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true,
-        locale: {
-            format: 'M/DD/YYYY'
-        }
-    });
-});
